c60c1b3647d4e7c267defb71651b6fd9437c791a
Umair Khan <omerjerk@gmail.com>
2014-01-12 15:34:34 +0530

    Change/Remove unused stuff
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- carpool/src/main/java/in/osc/carpool/MainActivity.java
+++ carpool/src/main/java/in/osc/carpool/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends ActionBarActivity
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        // BELOW CODE IS JUST FOR NOW
         switch (position) {
             case 0 : getSupportFragmentManager()
                     .beginTransaction()
@@ -71,13 +70,10 @@ public class MainActivity extends ActionBarActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = "Set home/destination";
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = "Search for carpool";
                 break;
         }
     }
