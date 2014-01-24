48f4fd5db8a80bb45ad825f926306587ab2370aa
Umair Khan <omerjerk@gmail.com>
2014-01-24 17:59:17 +0530

    class to refresh carpool database
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- carpool/src/main/java/in/osc/carpool/CarpoolSearchFragment.java
+++ carpool/src/main/java/in/osc/carpool/CarpoolSearchFragment.java
@@ -1,11 +1,32 @@
 package in.osc.carpool;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by omerjerk on 12/1/14.
@@ -41,9 +62,88 @@ public class CarpoolSearchFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Indicate that this fragment would like to influence the set of actions in the action bar.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // If the drawer is open, show the global app actions in the action bar. See also
+        // showGlobalContextActionBar, which controls the top-left area of the action bar.
+        inflater.inflate(R.menu.global, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         ((MainActivity) activity).onSectionAttached(
                 getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    private class RefreshCarpoolDatabase extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            String result = "";
+            try {
+                HttpClient client = new DefaultHttpClient();
+                String uri = "http://162.243.238.19/sauravtom/carpool.json";
+                URI website = new URI(uri);
+                HttpGet request = new HttpGet();
+                request.setURI(website);
+                HttpResponse response = client.execute(request);
+                HttpEntity entity = response.getEntity();
+                result = EntityUtils.toString(entity);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("[GET RESPONSE]", result);
+            File cacheFile = new File(getActivity().getFilesDir(), "data.json");
+
+            BufferedWriter bw = null;
+            try {
+                if (!cacheFile.exists()) {
+                    cacheFile.createNewFile();
+                }
+
+                FileWriter fw = new FileWriter(cacheFile.getAbsoluteFile());
+                bw = new BufferedWriter(fw);
+                bw.write(result);
+
+                Toast.makeText(getActivity(), "Latest Coupon codes downloaded !", Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e){
+                e.printStackTrace();
+                Toast.makeText(getActivity(), "Sorry! Something went wrong.", Toast.LENGTH_SHORT).show();
+            } finally {
+                try {
+                    bw.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //Should never happen
+                }
+
+            }
+        }
     }
 }
